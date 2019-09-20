5cdebfdceb0ea2cfccc682b863ed5f62b804ae1e
Elton Dornelas <eltondornelas@gmail.com>
2019-09-19 23:48:53 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/examplemongo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/examplemongo/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.examplemongo.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examplemongo.workshopmongo.domain.User;
+import com.examplemongo.workshopmongo.dto.UserDTO;
 import com.examplemongo.workshopmongo.services.UserService;
 
 @RestController //Recurso REST
@@ -20,7 +22,7 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET) //para dizer que esse método é o endpoint REST no caminho /users. Poderia utilizar o @GetMapping...	
-	public ResponseEntity<List<User>> findAll() { //o ResponseEntity ele retorna um objeto sofisticado que encapsula toda a estrutura necessária para retornar respostas HTTP já com cabeçalhos e erros...
+	public ResponseEntity<List<UserDTO>> findAll() { //o ResponseEntity ele retorna um objeto sofisticado que encapsula toda a estrutura necessária para retornar respostas HTTP já com cabeçalhos e erros...
 		//public List<User> findAll() {
 		//para fazer teste vamos instanciar alguns usuários
 		//User maria = new User("1", "Maria Brown", "maria@gmail.com");
@@ -30,7 +32,8 @@ public class UserResource {
 		List<User> list = service.findAll(); //busca no banco de dados os usuários para guardar na lista
 		//list.addAll(Arrays.asList(maria, alex));
 		
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); //converte cada objeto da lista para um DTO
+		return ResponseEntity.ok().body(listDto);
 		//o ok retorna o código de resposta em http de que ocorreu com sucesso
 		//body é o corpo da resposta. Vai ter a resposta list
 	}	
