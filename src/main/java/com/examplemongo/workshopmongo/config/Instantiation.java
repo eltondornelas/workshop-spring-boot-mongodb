5fdea05c7ffce31e11a7f2acd3b6da035d0b72a5
Elton Dornelas <eltondornelas@gmail.com>
2019-09-20 12:01:06 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/examplemongo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/examplemongo/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.examplemongo.workshopmongo.domain.Post;
 import com.examplemongo.workshopmongo.domain.User;
+import com.examplemongo.workshopmongo.dto.AuthorDTO;
 import com.examplemongo.workshopmongo.repository.PostRepository;
 import com.examplemongo.workshopmongo.repository.UserRepository;
 
@@ -35,12 +36,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		//nesse modelo estamos pegando uma cópia dos autores 
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		//com isso salva os usuários automaticamente no MongoDB
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		//nesse modelo estamos pegando uma cópia dos autores 
+				
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
